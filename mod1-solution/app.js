424884d868cce065e9cb4f575e5df30860435b9b
Your Name <you@example.com>
2020-09-24 14:26:45 +0530

    module 1-solution

---
+++ mod1-solution/app.js
@@ -0,0 +1,8 @@
+// (function () {
+//     'use strict';
+//     angular.module=('LunchCheak',[])
+//     .controller('LunchCheckController',LunchCheckController);
+//     LunchCheckController.$inject=['$scope'];
+    
+// })();
+!function(){"use strict";angular.module=[].controller("LunchCheckController",LunchCheckController),LunchCheckController.$inject=["$scope"]}()
