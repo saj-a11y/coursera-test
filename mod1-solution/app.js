a1016670f80bdb6a63651525d569eeed5f9c3003
Your Name <you@example.com>
2020-09-25 10:50:10 +0530

    small changes

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -1,8 +1,38 @@
-// (function () {
-//     'use strict';
-//     angular.module=('LunchCheak',[])
-//     .controller('LunchCheckController',LunchCheckController);
-//     LunchCheckController.$inject=['$scope'];
+(function () {
+    'use strict';
     
-// })();
-!function(){"use strict";angular.module=[].controller("LunchCheckController",LunchCheckController),LunchCheckController.$inject=["$scope"]}()
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.statusClass="";
+      $scope.message = "";
+      $scope.lunchDishes = "";
+      $scope.stateOfBeing = "hungry";
+    
+      $scope.checkDishes = function () {
+        var i = countDishes($scope.lunchDishes);
+        if (i==0){
+          $scope.statusClass="error";
+          $scope.message="Please enter data first";
+        }
+        else if(i>3){
+          $scope.statusClass="fine";
+          $scope.message="Too much!";
+        }
+        else{
+          $scope.statusClass="fine";
+          $scope.message="Enjoy!";
+        }
+      };
+    }
+    
+    function countDishes(string) {
+      // case of empty items between commas implemented
+      return string.split(',')
+          .filter(function(dish) { return dish.trim().length != 0 })
+          .length;
+    }
+    
+    })();
